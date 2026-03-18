2d47e4f8d2c9d46e389f9d7267af7621e9653717
Manikandan <manikandan.m.codes@gmail.com>
2026-03-18 15:21:45 +0530

    add gunicorn

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vyet3u&rqc%eyulj)_cw(*grll*xt%j)pg@5!(q6d$9!n3fnxe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
